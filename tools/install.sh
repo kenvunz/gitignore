d8e042a0b5475cf8221ad3aea31266c8cd70b5aa
Ken Vu <ken@gladeye.co.nz>
2013-03-19 08:45:23 +1300

    use curl instead

--- tools/install.sh
+++ tools/install.sh
@@ -1,5 +1,5 @@
 cd ~/
-wget -N https://raw.github.com/kenvunz/gitignore/master/.gitignore_global
+curl https://raw.github.com/kenvunz/gitignore/master/.gitignore_global > .gitignore_global
 # ignore for git in Tower.app
 /Applications/Tower.app/Contents/Resources/git/bin/git config --global core.excludesfile ~/.gitignore_global
 
