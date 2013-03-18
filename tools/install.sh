cf5398e792272b3550fdfb8961b310ce3889e6b8
Ken Vu <ken@gladeye.co.nz>
2013-03-18 20:49:21 +1300

    add in timestamp flag to avoid multiple downloads

--- tools/install.sh
+++ tools/install.sh
@@ -1,5 +1,5 @@
 cd ~/
-wget https://raw.github.com/kenvunz/gitignore/master/.gitignore_global
+wget -N https://raw.github.com/kenvunz/gitignore/master/.gitignore_global
 # ignore for git in Tower.app
 /Applications/Tower.app/Contents/Resources/git/bin/git config --global core.excludesfile ~/.gitignore_global
 
