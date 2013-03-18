e49ac619ebfcaa2a40053a029d43c9495c74ceda
Ken Vu <ken@gladeye.co.nz>
2013-03-18 20:36:57 +1300

    add the . before gitignore_global

--- tools/install.sh
+++ tools/install.sh
@@ -1,3 +1,3 @@
 cd ~/
-wget https://raw.github.com/kenvunz/gitignore/master/gitignore_global ~/.gitignore_global
+wget https://raw.github.com/kenvunz/gitignore/master/.gitignore_global
 /Applications/Tower.app/Contents/Resources/git/bin/git config --global core.excludesfile ~/.gitignore_global
