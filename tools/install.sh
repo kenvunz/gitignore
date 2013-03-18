9696aca9450ec3e9c262c9b496d02ddaf7390097
Ken Vu <ken@gladeye.co.nz>
2013-03-18 20:44:30 +1300

    include both git config for Tower.app and normal git

--- tools/install.sh
+++ tools/install.sh
@@ -1,3 +1,7 @@
 cd ~/
 wget https://raw.github.com/kenvunz/gitignore/master/.gitignore_global
+# ignore for git in Tower.app
 /Applications/Tower.app/Contents/Resources/git/bin/git config --global core.excludesfile ~/.gitignore_global
+
+# ignore for standard git
+git config --global core.excludesfile ~/.gitignore_global
