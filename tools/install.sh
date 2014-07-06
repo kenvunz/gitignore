634262d00d1a142b974925769b061ec3305d3935
Ken Vu <ken@gladeye.co.nz>
2014-07-06 14:26:38 +1200

    Updated path

--- tools/install.sh
+++ tools/install.sh
@@ -1,5 +1,5 @@
 cd ~/
-curl https://raw.github.com/kenvunz/gitignore/master/.gitignore_global > .gitignore_global
+curl https://raw.githubusercontent.com/kenvunz/gitignore/master/.gitignore_global > .gitignore_global
 # ignore for git in Tower.app
 /Applications/Tower.app/Contents/Resources/git/bin/git config --global core.excludesfile ~/.gitignore_global
 
